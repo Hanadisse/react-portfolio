0a58c88ae10600d2acd0a064655f5655a3cfcef5
hanad isse <hanadisse1@gmail.com>
2023-02-09 19:17:11 -0600

    took class names out and changed background

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,44 +1,37 @@
-
-import React from 'react';
-
+import React from "react";
 
 export default function AboutMe() {
   return (
     <div>
-      <div className='homePg-header' >
-
-        <h2 >About Me</h2>
+      <div>
+        <h2>About Me</h2>
       </div>
-      <div className="container">
-      <div className="card col imgCard" >
-          <div className="card-img">
-            <div className='meImgContainer'>
-          
-            </div>
-          </div>
-        </div>
-        <div className="card col" >
-          <div className="card-body">
-            <h5 className="card-title">Education</h5>
-            <p className="card-text">I am currently a student at University Of Minnesota Full-Stack Web Development Bootcamp. I was attending at inver hills community college for a econ transfer pathway to the U of MN. I graduate in Febuary 2023 to obtain a full stack developer certificate.</p>
-            <div className='imgContainer'>
-  
-            </div>
-          </div>
-        </div>
-        <div className="card col">
-          <div className="card-body">
-            <h5 className="card-title">Career Path</h5>
-            <p className="card-text"> I was working towards on obtaining a econimics degree and pursue a career somehwere with it. Eventually, I changed my career path and wanted to do something with tech. </p>
-          </div>
-        </div>
-        <div className="card col">
-          <div className="card-body">
-            <h5 className="card-title">Right Now</h5>
-            <p className="card-text"> I'm currently still in the process of being a better web developer and soon hope to msater skills that helps me be a great full stack web developer.
-           </p>
-          </div>
-        </div>
+
+      <h5>Education</h5>
+      <p>
+        I am currently a student at University Of Minnesota Full-Stack Web
+        Development Bootcamp. I was attending at inver hills community college
+        for a econ transfer pathway to the U of MN. I graduate in Febuary 2023
+        to obtain a full stack developer certificate.
+      </p>
+
+      <div>
+        <h5>Career Path</h5>
+        <p>
+          {" "}
+          I was working towards on obtaining a econimics degree and pursue a
+          career somehwere with it. Eventually, I changed my career path and
+          wanted to do something with tech.{" "}
+        </p>
+      </div>
+      <div>
+        <h5>Right Now</h5>
+        <p>
+          {" "}
+          I'm currently still in the process of being a better web developer and
+          soon hope to msater skills that helps me be a great full stack web
+          developer.
+        </p>
       </div>
     </div>
   );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,81 +4,67 @@ export default function Portfolio() {
   return (
     <div>
       <div>
-        <div>
-          <div>
-            <a
-              href="https://hanadisse.github.io/Password-Generator/"
-              className="link"
-            ></a>
-            <p className="caption">Password Generator</p>
-            <a href="https://github.com/Hanadisse/Password-Generator">
-              <button className="project-code-btn">Click me</button>
-            </a>
-          </div>
-        </div>
-        <div>
-          <div>
-            <a
-              href="https://hanadisse.github.io/Portfolio-1/"
-              className="link"
-            ></a>
-            <p className="caption">First Portfolio</p>
-            <a href="https://github.com/Hanadisse/Portfolio-1">
-              <button className="project-code-btn">Click me</button>
-            </a>
-          </div>
-        </div>
+        <a
+          href="https://hanadisse.github.io/Password-Generator/"
+          className="link"
+        ></a>
+        <p>Password Generator</p>
+        <a href="https://github.com/Hanadisse/Password-Generator">
+          <button>Click me</button>
+        </a>
       </div>
+
+      <div>
+        <a href="https://hanadisse.github.io/Portfolio-1/" className="link"></a>
+        <p>First Portfolio</p>
+        <a href="https://github.com/Hanadisse/Portfolio-1">
+          <button>Click me</button>
+        </a>
+      </div>
+
+      <div>
+        <a
+          href="https://hanadisse.github.io/java-code-quiz/"
+          className="link"
+        ></a>
+        <p>Java Code Quiz</p>
+        <a href="https://github.com/Hanadisse/java-code-quiz">
+          <button>Click me</button>
+        </a>
+      </div>
+
+      <div>
+        <a
+          href="https://serene-taiga-18669.herokuapp.com/"
+          className="link"
+        ></a>
+        <p>Food Tracker</p>
+        <a href="https://github.com/kipschetnan/Food-Tracker">
+          <button>Click me</button>
+        </a>
+      </div>
+
+      <div>
+        <a
+          href="https://deshawnwilliams94.github.io/Park-it/"
+          className="link"
+        ></a>
+        <p>Park it</p>
+        <a href="https://github.com/deshawnwilliams94/Park-it">
+          <button>Click me</button>
+        </a>
+      </div>
+
       <div>
         <div>
-          <div>
-            <a
-              href="https://hanadisse.github.io/java-code-quiz/"
-              className="link"
-            ></a>
-            <p className="caption2">Java Code Quiz</p>
-            <a href="https://github.com/Hanadisse/java-code-quiz">
-              <button className="project-code-btn2">Click me</button>
-            </a>
-          </div>
-        </div>
-        <div>
-          <div>
-            <a
-              href="https://serene-taiga-18669.herokuapp.com/"
-              className="link"
-            ></a>
-            <p className="caption2">Food Tracker</p>
-            <a href="https://github.com/kipschetnan/Food-Tracker">
-              <button className="project-code-btn2">Click me</button>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>
-            <a
-              href="https://deshawnwilliams94.github.io/Park-it/"
-              className="link"
-            ></a>
-            <p className="caption2">Park it</p>
-            <a href="https://github.com/deshawnwilliams94/Park-it">
-              <button className="project-code-btn2">Click me</button>
-            </a>
-          </div>
-        </div>
-        <div>
-          <div>
-            <a
-              href="https://hanadisse.github.io/City-weather-api/"
-              className="link"
-            ></a>
-            <p className="caption2">Weather Api</p>
-            <a href="https://github.com/Hanadisse/City-weather-api">
-              <button className="project-code-btn2">Click me</button>
-            </a>
-          </div>
+          <a
+            href="https://hanadisse.github.io/City-weather-api/"
+            className="link"
+          ></a>
+          <p>Weather Api</p>
+          <a href="https://github.com/Hanadisse/City-weather-api">
+            <button>Click me</button>
+          </a>
         </div>
       </div>
     </div>
