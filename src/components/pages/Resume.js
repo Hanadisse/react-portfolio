df798dc15e0bff82677df6dc525bce09a23bd68d
hanad isse <hanadisse1@gmail.com>
2023-02-09 18:47:04 -0600

    took classes out div i dont need

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,17 +4,12 @@ import Resumepdf from "../../assets/Resume.pdf"
 export default function Resume() {
     return (
       <div>
-        <div className="card-resume-pg2">
-          <div className="card-body">
-            <h4 className="card-title"> Resume:</h4>
+         <h4 className="card-title"> Resume:</h4>
             <a href= {Resumepdf} download= "resume.pdf" target= "_blank" rel="noreferrer"
             className="resume">
               <b>My Resume</b>
           
             </a>
-
-          </div>
-        </div>
       </div>
     );
   }
